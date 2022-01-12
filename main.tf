90421d7aa1aa302ec141e2d617a13dd5006b588d
mvinnicius <vinnicus@pop-os.localdomain>
2022-01-11 23:15:39 -0300

    Create security group

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  version = "~> 2.0"
+  region = "sa-east-1"
+}
+
+resource "aws_instance" "dev" {
+  count = 2
+  ami = "ami-090006f29ecb2d79a"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+      Name = "dev${count.index}"
+  }
+}
+
+resource "aws_security_group" "ssh-access"{
+    name = "ssh-access"
+    description = "Allow ssh for hosts communication"
+    ingress {
+        from_port = 22
+        to_port   = 22
+        protocol  = "tcp"
+        cidr_blocks = ["0.0.0.0/32"]
+    }
+
+    tags = {
+      Name = "ssh"
+    }
+}
